e083704ff075536591b5b78b5dfb25ed63f15bb1
Los4U <inz.kamil.wos@gmail.com>
2020-02-18 23:36:32 +0100

    lesson 31

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-servers',
@@ -11,12 +12,23 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
 
   allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = '';
 
   constructor() {
-    setTimeout(() => this.allowNewServer = true, 2000);
+    setTimeout(() => this.allowNewServer = true, 8000);
   }
 
   ngOnInit() {
   }
 
+  onCreatedServer() {
+    this.serverCreationStatus = 'Server was created: ' + this.serverName;
+  }
+
+
+  onUpdateServerName(event: Event) {
+    console.log(event);
+    this.serverName = (event.target as HTMLInputElement).value;
+  }
 }
